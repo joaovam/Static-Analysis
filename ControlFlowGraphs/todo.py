dafe2446d93b7fd0116750bf68fa427b55f0f6e0
joaovam <amorimvictorjoao3@gmail.com>
2024-03-25 19:07:05 -0300

    Finishing first assignment

--- ControlFlowGraphs/todo.py
+++ ControlFlowGraphs/todo.py
@@ -61,6 +61,28 @@ def test_min3(x, y, z):
         3
     """
     # TODO: Implement this method
+    env = Env({"x":x, "y":y, "z":z, "zero":0 })
+
+    x_min = Add("answer", "x", "zero")
+    y_min = Add("answer", "y", "zero")
+    z_min = Add("answer", "z", "zero")
+    
+    p1 = Lth("p","x", "y")
+
+    p2 = Lth("p", "x", "z")
+    x_or_z = Bt("p", x_min, z_min)
+    
+    p3 = Lth("p", "y", "z")
+    y_or_z = Bt("p", y_min, z_min)
+
+    i0 = Bt("p", p2, p3)
+
+    p1.add_next(i0)
+    p2.add_next(x_or_z)
+    p3.add_next(y_or_z)
+
+    interp(p1, env)
+
     return env.get("answer")
 
 
@@ -76,7 +98,29 @@ def test_div(m, n):
         >>> test_div(1, 3)
         0
     """
-    # TODO: Implement this method
+    env = Env({"m":m, "n":n, "zero": 0, "one":1, "count":0, "x":n})# 10/3 -> 3 6 9
+    
+    ret_count = Add("answer", "count", "zero")
+
+    init_division = Lth("p", "x", "m")
+    i1 = Add("x", "x", "n")
+
+    i0 = Bt("p", i1 ,ret_count)
+
+    i2 = Add("count", "count", "one")
+    
+     # primeiro teste
+
+    n_is_zero = Bt("n", init_division ,ret_count)#OK
+    m_is_zero = Bt("m", n_is_zero ,ret_count)#OK
+
+    i2.add_next(init_division)
+    i1.add_next(i2)
+    init_division.add_next(i0)
+    
+
+    interp(m_is_zero, env)
+
     return env.get("answer")
 
 
@@ -89,4 +133,22 @@ def test_fact(n):
         6
     """
     # TODO: Implement this method
+
+    #fact = 5 * 4 * 3 * 2 * 1
+    env = Env({"zero": 0, "one":1, "x":2,"count":1, "minus_1": -1, "n":n, "fact":1})
+
+    finish = Add("answer", "fact", "zero")
+
+    i1 = Geq("p", "count", "n")
+    fact = Mul("fact", "fact", "x")
+    i3 = Add("x", "x", "one")
+    i4 = Add("count", "count", "one")
+    i0 = Bt("p", finish, fact)
+
+    fact.add_next(i3)
+    i3.add_next(i4)
+    i4.add_next(i1)
+    i1.add_next(i0)
+
+    interp(i1, env)
     return env.get("answer")
